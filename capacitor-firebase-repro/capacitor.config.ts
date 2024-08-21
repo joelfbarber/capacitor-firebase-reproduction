ae9a3d7f2258a4e3b3ead39eddc9c24bf7029def
jbarber2018 <joel.barber@aarons.com>
2024-08-21 15:50:35 -0400

    added schemes

--- capacitor-firebase-repro/capacitor.config.ts
+++ capacitor-firebase-repro/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'capacitor-firebase-repro',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    scheme: "App QA"
+  }
 };
 
 export default config;
